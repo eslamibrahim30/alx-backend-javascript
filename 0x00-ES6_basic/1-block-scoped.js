90e60087173f2fe7753772127597540c6bab663b
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-03-04 23:27:26 +0000

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  task = false;
-  task2 = true;
+  let task;
+  let task2;
 
   if (trueOrFalse) {
-    var task;
-    var task2;
+    task = false;
+    task2 = true;
   }
 
+  task = false;
+  task2 = true;
   return [task, task2];
 }
