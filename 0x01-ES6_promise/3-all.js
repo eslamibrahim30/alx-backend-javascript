4baa00fa41151f465133f35bdd5a89a5b7e61749
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-03-06 23:56:27 +0000

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto, createUser]).then(
+  Promise.all([uploadPhoto(), createUser()]).then(
     () => {
       console.log('body firstName lastName');
     },
