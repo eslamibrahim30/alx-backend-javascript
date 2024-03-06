6124167139cb35964285ee0f2e1729e109d7ec6f
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-03-06 23:11:36 +0000

    Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,12 @@
-function done() {
-  console.log('Got a response from the API');
-  return { status: 200, body: 'Success' };
-}
-
-function failed() {
-  console.log('Got a response from the API');
-  return new Error();
-}
-
 export default function handleResponseFromAPI(promise) {
-  promise.then(done, failed);
+  promise.then(
+    () => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'Success' };
+    },
+    () => {
+      console.log('Got a response from the API');
+      return new Error();
+    },
+  );
 }
