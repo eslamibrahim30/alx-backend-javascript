5710e32f51d667d08e5b6a095f983b9378c19861
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-03-06 21:11:53 +0000

    Don't make a promise...if you know you can't keep it

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,6 @@
+export default function getFullResponseFromAPI(success) {
+  if (success) {
+    return Promise.resolve({ status: 200, body: 'Success' });
+  }
+  return Promise.resolve(Error('The fake API is not working currently'));
+}
